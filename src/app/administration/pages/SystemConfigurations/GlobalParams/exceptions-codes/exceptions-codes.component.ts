1fbc19050a6dc4343d77a1f45614d11062faef96
phelly <philndungu@gmail.com>
2022-03-01 14:29:40 +0300

    Updating

--- src/app/administration/pages/SystemConfigurations/GlobalParams/exceptions-codes/exceptions-codes.component.ts
+++ src/app/administration/pages/SystemConfigurations/GlobalParams/exceptions-codes/exceptions-codes.component.ts
@@ -157,11 +157,7 @@ export class ExceptionsCodesComponent implements OnInit {
             this.results = res;
             this.formData = this.fb.group({
               id:[this.results.id],
-<<<<<<< HEAD
               exception_code:[this.exception_code],
-=======
-              exception_code:[this.results.exception_code],
->>>>>>> 70e7bfa5032dd3866b5460a7aeba9623807eaf0c
             
               exce_description:[this.results.exce_description],
               exce_code_type:[this.results.exce_code_type],
@@ -220,7 +216,6 @@ export class ExceptionsCodesComponent implements OnInit {
               });
           })
         }
-<<<<<<< HEAD
         else if(this.function_type == "X-Delete"){
           this.isDeleting = true;
           this.isLookupDisabled = false;
@@ -234,18 +229,6 @@ export class ExceptionsCodesComponent implements OnInit {
               id:[this.results.id],
               exception_code:[this.exception_code],
             
-=======
-        else if(this.function_type == "X-Deleted"){
-          this.canVerify = false;
-          this.canDeleted = false;
-          this.isLookupDisabled = false;
-          this.isEnabled = true;
-          this.subscription = this.exceptionCodeApi.getException_codeId(this.exception_code).subscribe(res=>{
-            this.results = res;
-            this.formData = this.fb.group({
-              id:[this.results.id],
-              exception_code:[this.results.exception_code],
->>>>>>> 70e7bfa5032dd3866b5460a7aeba9623807eaf0c
               exce_description:[this.results.exce_description],
               exce_code_type:[this.results.exce_code_type],
               exce_working_class:[this.results.exce_working_class],
@@ -256,12 +239,6 @@ export class ExceptionsCodesComponent implements OnInit {
               exce_channel_exce:[this.results.exce_channel_exce],
               exce_is_verified:[this.results.exce_is_verified],
               exce_is_deleted:[this.results.exce_is_deleted],
-<<<<<<< HEAD
-=======
-              is_verified:[this.results.is_verified],
-              is_deleted:[true]
-              
->>>>>>> 70e7bfa5032dd3866b5460a7aeba9623807eaf0c
             });
           }, err=>{
             this.error = err;
@@ -273,11 +250,7 @@ export class ExceptionsCodesComponent implements OnInit {
                 panelClass: ['red-snackbar','login-snackbar'],
               });
           })
-<<<<<<< HEAD
         } 
-=======
-        }
->>>>>>> 70e7bfa5032dd3866b5460a7aeba9623807eaf0c
       })
       }
       // convenience getter for easy access to form fields
