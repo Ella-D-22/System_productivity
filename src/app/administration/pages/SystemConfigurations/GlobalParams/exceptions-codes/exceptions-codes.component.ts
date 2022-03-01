0a41546aa92236fb3a66b953fea7b4766cde02e0
phelly <philndungu@gmail.com>
2022-03-01 13:17:55 +0300

    Updates on various modules

--- src/app/administration/pages/SystemConfigurations/GlobalParams/exceptions-codes/exceptions-codes.component.ts
+++ src/app/administration/pages/SystemConfigurations/GlobalParams/exceptions-codes/exceptions-codes.component.ts
@@ -51,18 +51,7 @@ export class ExceptionsCodesComponent implements OnInit {
       // this.redirectToMaintenancePage();
       this.getPage();
     }
-    // redirectToMaintenancePage(){
-    //   this.subscription = this.exceptionCodeApi.changeMessage().subscribe(message=>{
-    //     this.message = message;
-    //     if( this.message == "default message"){
-    //       // Redirect to maintenace if no action header
-    //       this.ngZone.run(() => this.router.navigateByUrl('system/configurations/global/gl-subhead/maintenance'));
-    //     }else{
-    //       null;
-    //     }
-    //   })
-    // }
-
+    
       formData = this.fb.group({
         id:[''],
         exception_code: ['', [Validators.required]],
@@ -155,7 +144,7 @@ export class ExceptionsCodesComponent implements OnInit {
               duration: 3000,
               panelClass: ['red-snackbar','login-snackbar'],
             });
-            this.ngZone.run(() => this.router.navigateByUrl('system/event_id_module/maintenance'));
+            this.ngZone.run(() => this.router.navigateByUrl('system/configurations/global/exceptions-codes/maintenance'));
           })
         }
         else if(this.function_type == "M-Modify"){
@@ -166,7 +155,7 @@ export class ExceptionsCodesComponent implements OnInit {
           this.subscription = this.exceptionCodeApi.getException_codeId(this.exception_code).subscribe(res=>{
             this.results = res;
             this.formData = this.fb.group({
-              id:[''],
+              id:[this.results.id],
               exception_code:[this.exception_code],
             
               exce_description:[this.results.exce_description],
@@ -182,7 +171,7 @@ export class ExceptionsCodesComponent implements OnInit {
             });
           }, err=>{
             this.error = err;
-              this.ngZone.run(() => this.router.navigateByUrl('system/event_id_module/maintenance'));
+              this.ngZone.run(() => this.router.navigateByUrl('system/configurations/global/exceptions-codes/maintenance'));
               this._snackBar.open(this.error, "Try again!", {
                 horizontalPosition: this.horizontalPosition,
                 verticalPosition: this.verticalPosition,
@@ -197,11 +186,39 @@ export class ExceptionsCodesComponent implements OnInit {
           this.isLookupDisabled = false;
           // Populate data with rotected fileds only verification is enabled
         }
-        else if(this.function_type == "C-Cancel"){
+        else if(this.function_type == "X-Cancel"){
           this.canVerify = false;
           this.canDeleted = false;
           this.isLookupDisabled = false;
+          this.disabledFormControll()
           // should open a page with data and show remove button
+          this.subscription = this.exceptionCodeApi.getException_codeId(this.exception_code).subscribe(res=>{
+            this.results = res;
+            this.formData = this.fb.group({
+              id:[this.results.id],
+              exception_code:[this.exception_code],
+            
+              exce_description:[this.results.exce_description],
+              exce_code_type:[this.results.exce_code_type],
+              exce_working_class:[this.results.exce_working_class],
+              exce_work_class_min:[this.results.exce_work_class_min],
+              exce_authorization_matrix_id:[this.results.exce_authorization_matrix_id],
+              exce_gen_ledger_type:[this.results.exce_gen_ledger_type],
+              exce_ignore_exce_overriding_events:[this.results.exce_ignore_exce_overriding_events],
+              exce_channel_exce:[this.results.exce_channel_exce],
+              exce_is_verified:[this.results.exce_is_verified],
+              exce_is_deleted:[this.results.exce_is_deleted],
+            });
+          }, err=>{
+            this.error = err;
+              this.ngZone.run(() => this.router.navigateByUrl('system/configurations/global/exceptions-codes/maintenance'));
+              this._snackBar.open(this.error, "Try again!", {
+                horizontalPosition: this.horizontalPosition,
+                verticalPosition: this.verticalPosition,
+                duration: 3000,
+                panelClass: ['red-snackbar','login-snackbar'],
+              });
+          })
         } 
       })
       }
@@ -239,7 +256,7 @@ export class ExceptionsCodesComponent implements OnInit {
                     duration: 3000,
                     panelClass: ['green-snackbar','login-snackbar'],
                   });
-              this.ngZone.run(() => this.router.navigateByUrl('system/configurations/global/linked/organization/maintenance'));
+              this.ngZone.run(() => this.router.navigateByUrl('system/configurations/global/exceptions-codes/maintenance'));
                   // system/configurations/global/linked/organization/maintenance
               },err=>{
                 this.error = err;
