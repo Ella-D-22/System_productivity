7eef3b4d6bf9535b70727990e6e299549ad53696
chegejohn159 <chegejohn159@gmail.com>
2022-03-02 11:22:17 +0300

    02/03/2022

--- src/app/administration/pages/loan-account/lookup/branch/branch.service.ts
+++ src/app/administration/pages/loan-account/lookup/branch/branch.service.ts
@@ -10,7 +10,7 @@ import { environment } from 'src/environments/environment';
 })
 export class BranchService {
   //private baseUrl: string = 'http://127.0.0.1:8000/branches/';
-  private baseUrl: string = `${environment.systemAPI}/branches`;
+  private baseUrl: string = `${environment.systemAPI}/branch`;
 
   constructor(private http: HttpClient) { }
 
