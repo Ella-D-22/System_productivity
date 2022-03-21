7d66c3774bcd339a7b75a590a23cde1ac9adad15
phelly <philndungu@gmail.com>
2022-03-21 06:26:11 +0300

    limits

---
+++ src/app/administration/pages/Collaterals Service/limits/limits-maintenance/limits-maintenance.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-limits-maintenance',
+  templateUrl: './limits-maintenance.component.html',
+  styleUrls: ['./limits-maintenance.component.scss']
+})
+export class LimitsMaintenanceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
