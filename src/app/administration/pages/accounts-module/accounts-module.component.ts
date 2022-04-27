fae9297bf5d8c599be1aa29bbc217aaef7ab7c54
phelly <philndungu@gmail.com>
2022-04-27 18:07:48 +0300

    accounts commits

--- src/app/administration/pages/accounts-module/accounts-module.component.ts
+++ src/app/administration/pages/accounts-module/accounts-module.component.ts
@@ -1,4 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { CollateralLookupComponent } from '../collateral-limits/collateral/collateral-lookup/collateral-lookup.component';
+import { RetailCustomerLookupComponent } from '../CustomersComponent/retail-customer/retail-customer-lookup/retail-customer-lookup.component';
+import { LoanAccountService } from '../loan-account/loan-account.service';
+import { BranchComponent } from '../loan-account/lookup/branch/branch.component';
+import { productService } from '../loan-account/lookup/product/product.service';
+import { CurrentSchemeLookupComponent } from '../ProductModule/current-scheme/current-scheme-lookup/current-scheme-lookup.component';
+import { LoanproductLookupComponent } from '../ProductModule/loanproduct/loanproduct-lookup/loanproduct-lookup.component';
+import { OverdraftSchemeLookupComponent } from '../ProductModule/overdrafts-scheme/overdraft-scheme-lookup/overdraft-scheme-lookup.component';
+import { SavingschemeLookupComponent } from '../ProductModule/savings-scheme/savingscheme-lookup/savingscheme-lookup.component';
+import { TermDepositLookupComponent } from '../ProductModule/term-deposit/term-deposit-lookup/term-deposit-lookup.component';
+import { CurrencyLookupComponent } from '../SystemConfigurations/GlobalParams/currency-config/currency-lookup/currency-lookup.component';
+import { GlSubheadLookupComponent } from '../SystemConfigurations/GlobalParams/gl-subhead/gl-subhead-lookup/gl-subhead-lookup.component';
+import { MisSectorService } from '../SystemConfigurations/GlobalParams/mis-sector/mis-sector.service';
+import { AccountsService } from './accounts.service';
 
 @Component({
   selector: 'app-accounts-module',
@@ -7,9 +26,491 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountsModuleComponent implements OnInit {
 
-  constructor() { }
+  subscription:Subscription
+  horizontalPosition:MatSnackBarHorizontalPosition
+  verticalPosition:MatSnackBarVerticalPosition
+  sectorData:any
+  subSectorData:any
+  subSectors:any
+  currencyData:any
+  customer_lookup:any
+  customer_code:any
+  customer_name:any
+  imgfile:any
+  results:any
+  error:any
+  customerImage:any
+  signfile:any
+  signatureImage:any
+  isEnabled = false
+  newData = false;
+  lookupdata:any
+  glSubheads:any
+  filteredArr:any
+  tda_schemeCode:any
+  laa_schemeCode:any
+  sba_schemeCode:any
+  oda_schemeCode:any
+  caa_schemeCode:any
+  collateralData:any
+  element:any
+  constructor(private accountAPI:AccountsService,
+    private fb:FormBuilder,
+    private _snackBar:MatSnackBar,
+    private router:Router,
+    private dialog:MatDialog,
+    private misSectorAPI:MisSectorService,
+    private accountservice:LoanAccountService ) { }
 
   ngOnInit(): void {
+    this.getPage();
+    this.getMISData()
   }
 
+
+  glSubheadArray = new Array()
+  nomineeArray = new Array()
+  formData = this.fb.group({
+    accountBalance: [''],
+    accountManager: [''],
+    accountName: [''],
+    accountOwnership: [''],
+    accountStatus:[''],
+    accountType: [''],
+    acid: [''],
+    cashExceptionLimitCr: [''],
+    cashExceptionLimitDr: [''],
+    currency:[''],
+    customerCode: [''],
+    lienAmount: 0,
+    loan: new FormArray([]),
+    officeAccount: new FormArray([]),
+    openingDate: [''],
+    referredBy: [''],
+    saving: new FormArray([]),
+    sn: [''],
+    solCode: [''],
+    sectorCode:[''],
+    subSectorCode:[''],
+    termDeposit: new FormArray([]),
+    transferExceptionLimitCr:[''] ,
+    transferExceptionLimitDr:[''] ,
+    accountStatement: [''],
+    statementFreq:[''],
+    withholdingTax: [''],
+    postedBy: [''],
+    postedFlag: [''],
+    postedTime: [''],
+    modifiedBy: [''],
+    modifiedTime: [''],
+    verifiedBy: [''],
+    verifiedFlag: [''],
+    verifiedTime: [''],
+    deleteFlag: [''],
+    deleteTime: [''],
+    deletedBy: [''],
+    
+  })
+
+  loanData = this.fb.group({
+    laa_schemeCode:[''],
+    laa_glSubhead:[''],
+    acid: [''],
+    amountDisbursed: [0.000],
+    applicationStatus:[''],
+    collateralCode: [''],
+    demandCarryForward: [''],
+    disbursementDate:[''],
+    nextRepaymentDate: [''],
+    overFlowAmount: [''],
+    principalAccount: [''],
+    principalDemandAmount: [''],
+    repaymentPeriod: [''],
+    sn: [''],
+    sumPrincipalDemand: [''],
+    interestDemandAmount: [''],
+    loanDemands :new FormArray([]),
+    loanDocuments : new FormArray([])
+
+  })
+
+  loanDemandData = this.fb.group({
+    acid: [''],
+    adjustmentAmount: [''],
+    adjustmentDate:[''] ,
+    demandAmount: [''],
+    demandCode: [''],
+    demandDate: [''],
+    demandType:[''] ,
+    sn: ['']
+  })
+
+  loanDocumentData = this.fb.group({
+    documentImage: [''],
+    documentTitle: [''],
+    sn: [''],
+    uploadTime:['']
+  })
+
+  officeAccountData = this.fb.group({
+    accountHeadName: [''],
+    accountSupervisorId: [''],
+    id: ['']
+  })
+
+  savingsFormData = this.fb.group({
+    id: [''],
+    sba_maturedValue:[''],
+    nominees: new FormArray([]),
+    sba_savingPeriod: [''],
+    sba_schemeCode:[''],
+    sba_glSubhead:['']
+
+  })
+  
+
+  nomineesFormData = this.fb.group({
+    dob: [''],
+    emailAddress:[''] ,
+    firstName:[''],
+    id:[''] ,
+    identificationNo: [''],
+    lastName: [''],
+    middleName: [''],
+    nomineeMinor:[''] ,
+    occupation: [''],
+    phone: [''],
+    relationship: [''],
+    guardian: new FormArray([])
+  })
+
+  guardianFormData = this.fb.group({
+    address: [''],
+    guardianCode:[''],
+    guardianName: [''],
+    id:[''] ,
+    residence: ['']
+  })
+
+  termDepositData = this.fb.group({
+    breakDepInMultiples: [''],
+    depInstallmentAmount: [''],
+    depositPeriod: [''],
+    id: [''],
+    tda_maturedValue: [''],
+    tda_schemeCode:[''],
+    tda_glSubhead:[''],
+    nominees: new FormArray([]),
+  })
+  addGurantorsFormData = this.fb.group({
+
+  })
+  glSubheadData = this.fb.group({
+
+  })
+
+  get f(){return this.formData.controls}
+  get l(){return this.f.loan as FormArray}
+  // get d(){return this.loanDocumentData.controls}
+  // get ld(){return this.loanDemandData.controls}
+  get s(){return this.f.saving as FormArray}
+  // get n(){return this.nomineesFormData.controls}
+  get t(){return this.f.termDeposit as FormArray}
+  get oa(){return this.f.officeAccount as FormArray}
+  // get g(){return this.guardianFormData.controls}
+  get ll(){return this.loanData.controls}
+  get ss(){return this.savingsFormData.controls}
+  get n(){return this.ss.nominees as FormArray }
+
+  despatch_mode_array:any = [
+    'Post & E-Mail', 'Collect By Person', 'E-Mail Only', 'Post', 'No-Despatch', 'Courier', 'Courier & E-Mail'
+  ] 
+  statementFreqArray : any = [
+    'Daily', 'Weekly', 'Montly', 'Yearly'
+  ]
+  accountStatusArray: any = [
+    'Active', 'Not-Active', 'Dormant'
+  ]
+  aaplicationStatusAr : any = [
+    'Pending', 'Verified'
+  ]
+  relationshipArray:any = [
+    'FATHER', 'MOTHER', 'WIFE','HUSBAND', 'SIBLING', 'DAUGHTER', 'SON'
+  ]
+  guardianArray:any = [
+    'Court Appointed', 'Natural Guardian'
+  ]
+
+
+  initNomineeData(){
+    this.newData = true;
+    this.nomineesFormData = this.fb.group({
+      dob: [''],
+      emailAddress:[''] ,
+      firstName:[''],
+      id:[''] ,
+      identificationNo: [''],
+      lastName: [''],
+      middleName: [''],
+      nomineeMinor:[''] ,
+      occupation: [''],
+      phone: [''],
+      relationship: [''],
+      guardian: new FormArray([])
+    })
+  }
+
+  previewNominees(){
+    if(this.nomineesFormData.valid){
+      this.n.push(this.fb.group(
+        this.nomineesFormData.value
+      ));
+      this.nomineeArray.push(this.nomineesFormData.value);
+      this.initNomineeData();
+    }
+  }
+  onAddNomineesField(){
+    this.n.push(this.fb.group({
+      dob:[''],
+      emailAddress:[''],
+      firstName:[''],
+      identificationNo:[''],
+      lastName:[''],
+      middleName:[''],
+      occupation:[''],
+      phone:['']
+    }))
+  }
+  onNomineeClear(){
+    this.initNomineeData()
+    this.nomineeArray = new Array();
+  }
+  onUpdateNominee(){
+    let i = this.element
+    this.nomineeArray[i] = this.nomineesFormData.value
+  }
+  onRemove(i:any){
+    const index:number = this.nomineeArray.indexOf(this.nomineeArray.values);
+    this.nomineeArray.splice(index, i);
+    this.nomineeArray = this.nomineeArray
+
+  }
+ 
+  
+   //setting up the mis sector codes
+   getMISData(){
+    this.subscription = this.misSectorAPI.getAllMissectors().subscribe(
+      res =>{
+        this.sectorData = res
+      }
+    )
+  }
+  onInputSelection(event:any){
+  let miscode = event.target.value
+  console.log(miscode);
+  
+  this.subscription = this.misSectorAPI.getMissectorByCode(miscode).subscribe(
+    res =>{
+      this.subSectorData = res
+      this.subSectors = this.subSectorData.missubsectors
+    }
+  )
+  }
+
+  currencyLookup():void{
+    const dialogRef = this.dialog.open(CurrencyLookupComponent,{
+    });
+    dialogRef.afterClosed().subscribe(result =>{
+      this.currencyData = result.data;
+      this.formData.controls.currency.setValue(this.currencyData.ccy)
+    })
+ }
+
+ customerLookup(): void {
+  const dialogRef = this.dialog.open(RetailCustomerLookupComponent, {
+  });
+  dialogRef.afterClosed().subscribe(result => {
+    this.customer_lookup = result.data;
+    this.customer_code = this.customer_lookup.customerCode;
+    this.customer_name = this.customer_lookup.firstName +" "+this.customer_lookup.middleName +" "+this.customer_lookup.surname
+    this.formData.controls.accountManager.setValue(this.customer_lookup.firstName)
+    this.formData.controls.customerCode.setValue(this.customer_lookup.customerCode)
+  });
+}
+laaSchemeCodeLookup(): void {
+  const dialogRef = this.dialog.open(LoanproductLookupComponent, {
+  });
+  dialogRef.afterClosed().subscribe(result => {
+    this.lookupdata= result.data;
+    console.log("Loan Data", this.lookupdata);
+    this.formData.controls.schemeCode.setValue(this.lookupdata.laa_scheme_code);
+    this.laa_schemeCode = this.lookupdata.laa_scheme_code
+    this.glSubheads = this.lookupdata.laa_glsubheads
+    
+    this.filteredArr = this.glSubheads.filter(data => data.laa_gl_subhead_deafault == "Yes");
+    console.log(this.filteredArr);
+  });
+}
+caaSchemeCodeLookup():void{
+ const dialogRef = this.dialog.open(CurrentSchemeLookupComponent,{
+ });
+ dialogRef.afterClosed().subscribe(result =>{
+   this.lookupdata = result.data;
+   this.formData.controls.schemeCode.setValue(this.lookupdata.caa_scheme_code)
+   this.caa_schemeCode = this.lookupdata.caa_scheme_code
+
+   this.glSubheads = this.lookupdata.caa_glsubheads
+    
+   this.filteredArr = this.glSubheads.filter(data => data.caa_gl_subhead_deafault == "Yes");
+   console.log(this.filteredArr);
+ })
+}
+odaSchemeCodeLookup():void{
+  const dialogRef = this.dialog.open(OverdraftSchemeLookupComponent,{
+  });
+  dialogRef.afterClosed().subscribe(result =>{
+    this.lookupdata = result.data;
+    this.formData.controls.schemeCode.setValue(this.lookupdata.oda_scheme_code)
+    this.oda_schemeCode = this.lookupdata.oda_scheme_code
+    this.glSubheads = this.lookupdata.oda_glsubheads
+    
+    this.filteredArr = this.glSubheads.filter(data => data.oda_gl_subhead_deafault == "Yes");
+  })
+}
+sbaSchemeCodeLookup():void{
+  const dialogRef = this.dialog.open(SavingschemeLookupComponent,{
+  });
+  dialogRef.afterClosed().subscribe(result =>{
+    this.lookupdata = result.data;
+    this.formData.controls.schemeCode.setValue(this.lookupdata.sba_scheme_code)
+
+    this.sba_schemeCode = this.lookupdata.sba_scheme_code
+    this.glSubheads = this.lookupdata.sba_glsubheads
+    
+    this.filteredArr = this.glSubheads.filter(data => data.sba_gl_subhead_deafault == "Yes");
+  })
+}
+tdaSchemeCodeLookup():void{
+  const dialogRef = this.dialog.open(TermDepositLookupComponent,{
+  });
+  dialogRef.afterClosed().subscribe(result =>{
+    this.lookupdata = result.data;
+    this.formData.controls.schemeCode.setValue(this.lookupdata.tda_scheme_code)
+
+    this.tda_schemeCode = this.lookupdata.tda_scheme_code
+    this.glSubheads = this.lookupdata.tda_glsubheads
+    
+    this.filteredArr = this.glSubheads.filter(data => data.tda_gl_subhead_deafault == "Yes");
+  })
+}
+collateralLookup():void{
+  const dialogRef = this.dialog.open(CollateralLookupComponent,{})
+  dialogRef.afterClosed().subscribe(result =>{
+    this.collateralData = result.data
+    this.loanData.controls.collateralCode.setValue(this.collateralData)
+  })
+}
+
+guarantorsCustomerLookup(): void {
+  const dialogRef = this.dialog.open(RetailCustomerLookupComponent, {
+  });
+  dialogRef.afterClosed().subscribe(result => {
+    this.customer_lookup = result.data;
+    this.customer_code = this.customer_lookup.customerCode;
+    this.customer_name = this.customer_lookup.firstName +" "+this.customer_lookup.middleName +" "+this.customer_lookup.surname
+    this.addGurantorsFormData.controls.customer_code.setValue(this.customer_code);
+  });
+}
+
+//Checking for eligibility of a guarantors customer_code
+eligibilityTest(){
+  if(this.addGurantorsFormData.valid){
+    let customer_code = this.addGurantorsFormData.controls.customer_code.value;
+    this.accountservice.getCustomerEligibility(customer_code).subscribe(
+      res =>{
+          this.results = res
+            this.glSubheadArray.push(this.glSubheadData.value);
+      },
+      err=>{
+        this.error = err.error
+        this._snackBar.open(this.error.message, "Try Another",{
+          horizontalPosition:this.horizontalPosition,
+          verticalPosition:this.verticalPosition,
+          duration:3000,
+          panelClass:['red-snackbar', 'login-snackbar']
+        })
+      }
+    )
+  }else{
+    this._snackBar.open("Invalid Form Data", "Try Again",{
+      horizontalPosition:this.horizontalPosition,
+      verticalPosition:this.verticalPosition,
+      duration:3000,
+      panelClass:['red-snackbar', 'login-snackbar']
+    })
+  }
+
+}
+
+onPhotoChange(event: any) {
+  this.imgfile = event.target.files[0];
+    if (event.target.files && event.target.files[0]) {
+                  var reader = new FileReader();
+          reader.readAsDataURL(event.target.files[0]);
+reader.onload = () => {
+this.customerImage=reader.result;
+console.log(this.customerImage)
+}
+reader.onerror = function (error) {
+console.log('Error: ', error);
+};
+
+}
+
+}
+
+onSignatureChange(event: any) {
+this.signfile = event.target.files[0];
+
+  if (event.target.files && event.target.files[0]) {
+
+                      var reader = new FileReader();
+        reader.readAsDataURL(event.target.files[0]);
+reader.onload = () => {
+this.signatureImage=reader.result;
+console.log(this.signatureImage)
+}
+reader.onerror = function (error) {
+console.log('Error: ', error);
+};
+
+}
+}
+
+glSubheadLookup1(): void {
+const dialogRef = this.dialog.open(GlSubheadLookupComponent, {
+
+});
+dialogRef.afterClosed().subscribe(result => {
+  this.formData.controls.glSubhead.setValue(result.data.glSubheadCode);
+});
+}
+
+branchSubheadLookup(): void {
+const dialogRef = this.dialog.open(BranchComponent, {
+
+});
+dialogRef.afterClosed().subscribe(result => {
+  this.formData.controls.solCode.setValue(result.data.solCode);
+});
+}
+
+getPage(){
+
+}
+
+
+onSubmit(){
+
+}
 }
