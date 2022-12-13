28ad303a71fc08015498e02ab2e3a36597a9ad40
chegejohn159 <chegejohn159@gmail.com>
2022-12-13 17:34:40 +0300

    id error

--- src/app/administration/administration-routing.module.ts
+++ src/app/administration/administration-routing.module.ts
@@ -18,11 +18,11 @@ const routes: Routes = [
         path: '',
         component: DashboardComponent,
       },
-      {
-        path: 'add-reports',
-        component: ReportsComponent,
+      // {
+      //   path: 'add-reports',
+      //   component: ReportsComponent,
        
-      },
+      // },
       {
         path: 'view-reports',
         component: ViewReportsComponent,
